a008d8036107a0a9eccd8daf52c75f6206dd8eac
fisherm4n <4143322@inbox.ru>
2020-12-03 17:15:24 +0300

    trying with mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,13 @@ $(function (){
     arrows:false,
     fade:true
   });
+  
+  var mixer = mixitup(products__items, {
+    selectors: {
+        target: '.products__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
 });
